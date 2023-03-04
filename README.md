9c5812a0296621a8e2b2589aa452e0ca625248a8
Nunsinee Viken <71260513+nunsinee@users.noreply.github.com>
2023-03-04 07:48:56 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mobile-tab-navigation
-Build mobile tab navigation
+
+Build Mobile Tab navigation
+
+## Screenshort
+
+![image](./images/Screenshot_01.png)
+![image](./images/Screenshot_02.png)
+
+## Description
+
+Use JavaScript to tab on menu and change image
+
+### Built with JavaScript,HTML and CSS
+
+#### Referense
+
+Learn from Udemy course by Brad Traversy
---
+++ js/script.js
@@ -0,0 +1,23 @@
+const contents = document.querySelectorAll(".content");
+const listItems = document.querySelectorAll("nav ul li");
+
+listItems.forEach((item, index) => {
+	item.addEventListener("click", () => {
+		hideAllContents();
+		hideAllItems();
+		item.classList.add("active");
+		contents[index].classList.add("show");
+	});
+});
+
+const hideAllContents = () => {
+	contents.forEach((content) => {
+		content.classList.remove("show");
+	});
+};
+
+const hideAllItems = () => {
+	listItems.forEach((item) => {
+		item.classList.remove("active");
+	});
+};
